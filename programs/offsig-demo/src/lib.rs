2e98dd12fb0d83e29d639be453c6defe247efea9
rocalex <rocmaildev@gmail.com>
2022-05-16 14:49:25 +0300

    get message from ed25519 instruction

--- programs/offsig-demo/src/lib.rs
+++ programs/offsig-demo/src/lib.rs
@@ -1,38 +1,122 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program;
+use byteorder::ByteOrder;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+struct Ed25519InstructionPart<'a> {
+    address: &'a [u8],
+    msg_offset: usize,
+    msg_size: usize,
+}
+
+fn validate_action(instruction_acc: &AccountInfo, my_account: &Account<MyAccount>) -> Result<Vec<u8>> {
+    let current_instruction =
+        solana_program::sysvar::instructions::load_current_index_checked(&instruction_acc)?;
+    if current_instruction == 0 {
+        return Err(ErrorCode::InstructionAtWrongIndex.into());
+    }
+    
+    // The previous ix must be a ed25519 verification instruction
+    let ed25519_ix_index = (current_instruction - 1) as u16;
+    let ed25519_ix = match solana_program::sysvar::instructions::load_instruction_at_checked(
+        ed25519_ix_index as usize,
+        &instruction_acc,
+    ) {
+        Ok(ix) => ix,
+        Err(_) => return Err(ErrorCode::InvalidEd25519Instruction.into()),
+    };
+
+    // Check that the instruction is actually for the ed25519 program
+    if ed25519_ix.program_id != solana_program::ed25519_program::id() {
+        return Err(ErrorCode::InvalidProgramId.into());
+    }
+
+    let ed25519_data_len = ed25519_ix.data.len();
+    if ed25519_data_len < 2 {
+        return Err(ErrorCode::InvalidEd25519Instruction.into());
+    }
+    let sig_len = ed25519_ix.data[0];
+    let mut index: usize = 0 // count and padding
+        + 2 // signature_offset
+        + 2 // signature_instruction_index
+        + 2; // public_key_offset
+    let mut ed25519_ixs: Vec<Ed25519InstructionPart> = Vec::with_capacity(sig_len as usize);
+
+    for _ in 0..sig_len {
+        let address_offset = byteorder::LE::read_u16(&ed25519_ix.data[index..index + 2]) as usize;
+        let address: &[u8] = &ed25519_ix.data[address_offset..address_offset + 32];
+        index += 4;
+        let msg_offset = byteorder::LE::read_u16(&ed25519_ix.data[index..index + 2]) as usize;
+        index += 2;
+        let msg_size = byteorder::LE::read_u16(&ed25519_ix.data[index..index + 2]) as usize;
+        ed25519_ixs.push(Ed25519InstructionPart {
+            address,
+            msg_offset,
+            msg_size,
+        });
+    }
+
+    // Extract message which is encoded in Solana Secp256k1 instruction data.
+    let message = &ed25519_ix.data
+        [ed25519_ixs[0].msg_offset..(ed25519_ixs[0].msg_offset + ed25519_ixs[0].msg_size)];
+
+    if my_account.group_key != ed25519_ixs[0].address {
+        return Err(ErrorCode::InvalidGroupKey.into());
+    }
+    Ok(message.to_vec())
+}
 
 #[program]
 pub mod offsig_demo {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        
+
+    pub fn initialize(ctx: Context<Initialize>, group_key: [u8; 32]) -> Result<()> {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.group_key = group_key;
         Ok(())
     }
 
-    pub fn verify(ctx: Context<VerifyOffsig>, msg_array: [u8; 32], sig_array: [u8; 64], pub_array: [u8; 65]) -> ProgramResult {
-        let message = libsecp256k1::Message::parse(&msg_array);
-        let signature = match libsecp256k1::Signature::parse_standard(&sig_array) {
-            Ok(s) => s,
-            Err(e) => return Err(ErrorCode::Hello.into()),
-        };
-        let pub_key = match libsecp256k1::PublicKey::parse(&pub_array) {
-            Ok(p) => p,
-            Err(e) => return Err(ErrorCode::Hello.into()),
-        };
-        libsecp256k1::verify(&message, &signature, &pub_key);
+    pub fn verify(ctx: Context<VerifyOffsig>) -> Result<()> {
+        let my_account = &mut ctx.accounts.my_account;
+        let message = validate_action(&ctx.accounts.instruction_acc, my_account)?;
+
+        msg!("Message: {:?}", message);
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[account]
+#[derive(Default)]
+pub struct MyAccount {
+    group_key: [u8; 32],
+}
 
 #[derive(Accounts)]
-pub struct VerifyOffsig {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 32)]
+    pub my_account: Account<'info, MyAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
 
-#[error]
+#[derive(Accounts)]
+pub struct VerifyOffsig<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>,
+    /// CHECK:
+    pub instruction_acc: AccountInfo<'info>,
+}
+
+#[error_code]
 pub enum ErrorCode {
-    #[msg("This is an error message clients will automatically display")]
-    Hello,
+    #[msg("instruction at wrong index")]
+    InstructionAtWrongIndex,
+    #[msg("invalid ed25519 instruction")]
+    InvalidProgramId,
+    #[msg("invalid ed25519 instruction")]
+    InvalidEd25519Instruction,
+    #[msg("invalid group key")]
+    InvalidGroupKey,
 }
--- tests/offsig-demo.ts
+++ tests/offsig-demo.ts
@@ -1,29 +1,67 @@
-import { randomBytes } from 'crypto';
+import assert from 'assert'
+import * as ed from '@noble/ed25519'
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
-import * as secp256k1 from 'secp256k1'
 import { OffsigDemo } from '../target/types/offsig_demo';
 
 describe('offsig-demo', () => {
 
-  let provider = anchor.Provider.env()
-  // Configure the client to use the local cluster.
-  anchor.setProvider(provider);
+    let provider = anchor.Provider.env()
+    // Configure the client to use the local cluster.
+    anchor.setProvider(provider);
 
-  const program = anchor.workspace.OffsigDemo as Program<OffsigDemo>;
+    const program = anchor.workspace.OffsigDemo as Program<OffsigDemo>;
 
-  it('Is initialized!', async () => {
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
-  });
+    let privateKey: Uint8Array;
+    let groupKey: Uint8Array;
+    let myAccount: anchor.web3.Keypair;
 
-  it('Is verified!', async () => {
-    const msg = randomBytes(32)
-    let privKey = randomBytes(32)
-    let sigObj = secp256k1.ecdsaSign(msg, privKey)
-    let pubKey = secp256k1.publicKeyCreate(privKey, false)
+    before(async () => {
+        privateKey = ed.utils.randomPrivateKey();
+        groupKey = await ed.getPublicKey(privateKey);
+    })
 
-    const tx = await program.rpc.verify([...msg], [...sigObj.signature], [...pubKey], {});
-    console.log("Your transaction signature", tx);
-  });
+    it('Is initialized!', async () => {
+        myAccount = anchor.web3.Keypair.generate()
+        const tx = await program.rpc.initialize(Array.from(groupKey), {
+            accounts: {
+                myAccount: myAccount.publicKey,
+                user: provider.wallet.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            },
+            signers: [myAccount]
+        });
+        console.log("Your transaction signature", tx);
+
+        const myAccountAccount = await program.account.myAccount.fetch(myAccount.publicKey);
+        const storedGroupKey = Buffer.from(myAccountAccount.groupKey)
+        assert.ok(storedGroupKey.equals(groupKey))
+    });
+
+    it('Is verified in frontend!', async () => {
+        const message = Uint8Array.from([0xab, 0xbc, 0xcd, 0xde]);
+        const signature = await ed.sign(message, privateKey);
+        const isValid = await ed.verify(signature, message, groupKey);
+        assert.ok(isValid == true)
+    });
+
+    it('Is verified on chain', async () => {
+        const message = Uint8Array.from([0xab, 0xbc, 0xcd, 0xde]);
+        const signature = await ed.sign(message, privateKey);
+        const verifyInstruction = anchor.web3.Ed25519Program.createInstructionWithPublicKey({
+            publicKey: groupKey,
+            message: message,
+            signature: signature
+        })
+        const programInstruction = program.instruction.verify({
+            accounts: {
+                myAccount: myAccount.publicKey,
+                instructionAcc: anchor.web3.SYSVAR_INSTRUCTIONS_PUBKEY
+            }
+        })
+        const txn = new anchor.web3.Transaction()
+        txn.add(verifyInstruction, programInstruction)
+        const tx = await provider.send(txn)
+        console.log('transaction signature:', tx);
+    })
 });
